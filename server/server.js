c29c3ddff2bb6525e2b0cf15ee6fdbb42f389423
DiLytics GitHub <35942227+Dilgithub@users.noreply.github.com>
2018-08-07 12:36:41 +0530

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
+
 const express=require('express');
 const bodyParser=require('body-parser');
-const _=require('lodash');
-var {ObjectID}=require('mongodb');
+const _ =require('lodash');
 
+const    {ObjectID}=require('mongodb');
 var {mongoose}=require('./db/mongoose');
 var {Todo}=require('./models/todo');
 var {User}=require('./models/user');
@@ -50,6 +51,53 @@ Todo.find().then((todo)=>{
         res.status(400).send();
     });
  });
+
+ app.delete('/todos/:id',(req,res)=>{
+      var id=req.params.id;
+       if(!ObjectID.isValid(id)){
+             return res.status(404).send();
+       }
+       Todo.findByIdAndRemove(id).then((todo)=>{
+            if(!todo){
+                return res.status(404).send();
+            }
+            else{
+                res.send(todo);
+            }
+        }).catch((e)=>{
+            res.status(400).send();
+        });
+ });
+    
+ app.patch('/todos/:id',(req,res)=>{
+     console.log("body",req.body);
+        var id=req.params.id;
+        var body=_.pick(req.body,['text','completed']);
+        if(!ObjectID.isValid(id)){
+            console.log("error check")
+            return res.status(404).send();
+        }
+    if(_.isBoolean(body.completed && body.completed)){
+           body.completedAt=new Date().getTime();
+    }
+    else{
+           body.completed=false;
+           body.completedAt=null;
+    }
+       Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo)=>{
+                if(!todo){
+                    return res.status(404).send();
+                }
+                res.send(todo);
+       }).catch((err)=>{
+              res.status(400).send()
+       });
+
+       
+    
+          
+ });
+
 module.exports={app}
 
 
